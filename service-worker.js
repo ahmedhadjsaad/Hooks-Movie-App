c53bb03cd9bc9c241a7cc866bae75aa936680d38
unknown <ahmed.hadjsaad@iit.ens.tn>
2020-10-01 14:19:18 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Hooks-Movie-App.git/precache-manifest.0c0bce5318e456aca7e2119e10756efe.js"
+  "/Hooks-Movie-App/precache-manifest.9f938eec1fba5d80224e52fb765a3511.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hooks-Movie-App.git/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Hooks-Movie-App/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
